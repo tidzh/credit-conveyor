2d3cc8fae203e4c403811a1e0c3883a4cfe85a60
Кондаков Александр Олегович <aokondakov@dev.vtb.ru>
2022-05-01 12:40:48 +0300

    refactor: refactoring app

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
 
 const app = express();
+const port = process.env.PORT || '3000'
+
+app.set('port', port);
+
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
+
+
